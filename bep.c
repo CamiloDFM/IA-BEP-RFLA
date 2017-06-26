10248b26f14bfb2fef0c9a31589896829d3a0ee6
Camilo <CamiloDFM@users.noreply.github.com>
2017-06-26 04:51:11 -0400

    leer datos de la instancia

--- bep.c
+++ bep.c
@@ -12,32 +12,89 @@ int main(int argc, char* argv[]){
     
     FILE* instancia = fopen(argv[1], "r");
     
-    // los datos de la instancia estan en el nombre de archivo
-    
-    int nEstaciones, nPuntos, nRefugios, nBuses;
-    sscanf(argv[1], "InstanciasBEP/InstanceBEP-%d-%d-%d-%d.txt", &nEstaciones, &nPuntos, &nRefugios, &nBuses);
-    
     // leer las capacidades
     
     // buses
-    int skip, capacidadBuses;
-    fscanf(instancia, "%d: %d\n", &skip, &capacidadBuses);
+    
+    int nBuses, capacidadBuses;
+    // nBuses: cantidad de buses en juego,
+    // capacidadBuses: capacidad máxima de cada bus
+    fscanf(instancia, "%d: %d\n", &nBuses, &capacidadBuses);
     
     // estaciones
+    int i, nEstaciones;
+    // nEstaciones: cantidad de estaciones de partida de buses
+    fscanf(instancia, "%d: ", &nEstaciones);
     int* busesEstacion = (int*)malloc(sizeof(int)*nEstaciones);
-    int i;
-    fscanf(instancia, "%d: ", &skip);
+    // busesEstacion[i]: cantidad de buses que parten en la estacion i
     for(i = 0; i < nEstaciones; i++){
         fscanf(instancia, "%d", &(busesEstacion[i]));
     }
     
     // puntos
-    int personasTotales;
-    fscanf(instancia, "\n%d: %d: ", &skip, &personasTotales);
+    int nPuntos, personasTotales;
+    // nPuntos: cantidad de puntos de recogida de personas,
+    // personasTotales: cantidad total de gente a ser evacuada
+    fscanf(instancia, "\n%d: %d: ", &nPuntos, &personasTotales);
+    int* personasPunto = (int*)malloc(sizeof(int)*nPuntos);
+    // personasPunto[i]: cantidad de personas a recoger en el punto i
+    for(i = 0; i < nPuntos; i++){
+        fscanf(instancia, "%d", &(personasPunto[i]));
+    }
     
+    // refugios
+    int nRefugios, capacidadTotal;
+    // nRefugios: cantidad de refugios / sumideros para el problema,
+    // capacidadTotal: suma de las capacidades máximas de cada refugio
+    fscanf(instancia, "\n%d: %d: ", &nRefugios, &capacidadTotal);
+    int* capacidadRefugio = (int*)malloc(sizeof(int)*nRefugios);
+    // capacidadRefugio[i]: capacidad máxima del refugio i
+    for(i = 0; i < nRefugios; i++){
+        fscanf(instancia, "%d", &(capacidadRefugio[i]));
+    }
+    
+    // leer distancias entre puntos, estaciones y refugios
+    
+    // distancias entre estaciones y puntos
+    int** distanciasEstacionPunto = (int**)malloc(sizeof(int*)*nEstaciones);
+    for(i = 0; i < nEstaciones; i++){
+        distanciasEstacionPunto[i] = (int*)malloc(sizeof(int)*nPuntos);
+    } 
+    // distanciasEstacionPunto[i][j]: distancia entre la estacion inicial i y el punto de recogida j
+    
+    int j, skip;
+    for(i = 0; i < nEstaciones; i++){
+        fscanf(instancia, "%d: ", &skip);
+        for(j = 0; j < nPuntos; j++){
+            fscanf(instancia, "%d", &(distanciasEstacionPunto[i][j]));
+        }
+    }
+    
+    // distancias entre puntos y refugios
+    int** distanciasPuntoRefugio = (int**)malloc(sizeof(int*)*nPuntos);
+    for(i = 0; i < nPuntos; i++){
+        distanciasPuntoRefugio[i] = (int*)malloc(sizeof(int)*nRefugios);
+    } 
+    // distanciasPuntoRefugio[i][j]: distancia entre el punto de recogida i y el refugio j
+
+    for(i = 0; i < nPuntos; i++){
+        fscanf(instancia, "%d: ", &skip);
+        for(j = 0; j < nRefugios; j++){
+            fscanf(instancia, "%d", &(distanciasPuntoRefugio[i][j]));
+        }
+    }
+    
+    // cleanup
+    fclose(instancia);
     
     // test
-    printf("%d\n", personasTotales);
+    
+    for(i = 0; i < nPuntos; i++){
+        for(j = 0; j < nRefugios; j++){
+            printf("%d ", distanciasPuntoRefugio[i][j]);
+        }
+        printf("\n");
+    }
     
 	return 0;
 }
